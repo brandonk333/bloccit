cfc8b9b120e607e9286b6bb9994ed210ee691940
btkunisch <btkunisch_@hotmail.com>
2015-12-06 00:48:26 +0000

    assignment-32-crud seeded and completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,17 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+
+50.times do
+  Advertisement.create!(
+    title:  RandomData.random_sentence,
+    copy:   RandomData.random_paragraph,
+    price:  99
+  )
+end
+
  
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
