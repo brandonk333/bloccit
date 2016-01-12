7fb84455d16a08339f172c9623f0aed9480d46c9
btkunisch <btkunisch_@hotmail.com>
2016-01-12 20:09:22 +0000

    checkpoint-47-ajax completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,28 +4,35 @@ class CommentsController < ApplicationController
  
   def create
     @post = Post.find(params[:post_id])
-    comment = @post.comments.new(comment_params)
-    comment.user = current_user
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    @new_comment = Comment.new
  
-    if comment.save
+    if @comment.save
       flash[:notice] = "Comment saved successfully."
-      redirect_to [@post.topic, @post]
     else
       flash[:alert] = "Comment failed to save."
-      redirect_to [@post.topic, @post]
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
   
   def destroy
     @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
+    @comment = @post.comments.find(params[:id])
  
-    if comment.destroy
+    if @comment.destroy
       flash[:notice] = "Comment was deleted."
-      redirect_to [@post.topic, @post]
     else
       flash[:alert] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
   
@@ -33,7 +40,6 @@ class CommentsController < ApplicationController
     comment = Comment.find(params[:id])
     unless current_user == comment.user || current_user.admin?
       flash[:alert] = "You do not have permission to delete a comment."
-      redirect_to [comment.post.topic, comment.post]
     end
   end
  
